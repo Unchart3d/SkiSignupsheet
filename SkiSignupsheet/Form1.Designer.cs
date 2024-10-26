adbbdfac3e6fc4c2e0d7c8ad26f59e68366b900c
ianni <ianni@UNCHARTED>
2024-10-25 20:38:21 -0500

    Add project files.

---
+++ SkiSignupsheet/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace SkiSignupsheet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.snowgloves = new System.Windows.Forms.CheckBox();
+            this.skis = new System.Windows.Forms.CheckBox();
+            this.goggles = new System.Windows.Forms.CheckBox();
+            this.earmuffs = new System.Windows.Forms.CheckBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.cartToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearOrderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitApplicationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // snowgloves
+            // 
+            this.snowgloves.AutoSize = true;
+            this.snowgloves.Location = new System.Drawing.Point(23, 53);
+            this.snowgloves.Name = "snowgloves";
+            this.snowgloves.Size = new System.Drawing.Size(89, 17);
+            this.snowgloves.TabIndex = 0;
+            this.snowgloves.Text = "Snow Gloves";
+            this.snowgloves.UseVisualStyleBackColor = true;
+            // 
+            // skis
+            // 
+            this.skis.AutoSize = true;
+            this.skis.Location = new System.Drawing.Point(23, 106);
+            this.skis.Name = "skis";
+            this.skis.Size = new System.Drawing.Size(46, 17);
+            this.skis.TabIndex = 1;
+            this.skis.Text = "Skis";
+            this.skis.UseVisualStyleBackColor = true;
+            // 
+            // goggles
+            // 
+            this.goggles.AutoSize = true;
+            this.goggles.Location = new System.Drawing.Point(23, 159);
+            this.goggles.Name = "goggles";
+            this.goggles.Size = new System.Drawing.Size(65, 17);
+            this.goggles.TabIndex = 2;
+            this.goggles.Text = "Goggles";
+            this.goggles.UseVisualStyleBackColor = true;
+            // 
+            // earmuffs
+            // 
+            this.earmuffs.AutoSize = true;
+            this.earmuffs.Location = new System.Drawing.Point(23, 212);
+            this.earmuffs.Name = "earmuffs";
+            this.earmuffs.Size = new System.Drawing.Size(67, 17);
+            this.earmuffs.TabIndex = 3;
+            this.earmuffs.Text = "Earmuffs";
+            this.earmuffs.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(375, 53);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(309, 296);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cartToolStripMenuItem,
+            this.clearOrderToolStripMenuItem,
+            this.exitApplicationToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // cartToolStripMenuItem
+            // 
+            this.cartToolStripMenuItem.Name = "cartToolStripMenuItem";
+            this.cartToolStripMenuItem.Size = new System.Drawing.Size(41, 20);
+            this.cartToolStripMenuItem.Text = "Cart";
+            this.cartToolStripMenuItem.Click += new System.EventHandler(this.cartToolStripMenuItem_Click);
+            // 
+            // clearOrderToolStripMenuItem
+            // 
+            this.clearOrderToolStripMenuItem.Name = "clearOrderToolStripMenuItem";
+            this.clearOrderToolStripMenuItem.Size = new System.Drawing.Size(79, 20);
+            this.clearOrderToolStripMenuItem.Text = "Clear Order";
+            this.clearOrderToolStripMenuItem.Click += new System.EventHandler(this.clearOrderToolStripMenuItem_Click);
+            // 
+            // exitApplicationToolStripMenuItem
+            // 
+            this.exitApplicationToolStripMenuItem.Name = "exitApplicationToolStripMenuItem";
+            this.exitApplicationToolStripMenuItem.Size = new System.Drawing.Size(102, 20);
+            this.exitApplicationToolStripMenuItem.Text = "Exit Application";
+            this.exitApplicationToolStripMenuItem.Click += new System.EventHandler(this.exitApplicationToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.earmuffs);
+            this.Controls.Add(this.goggles);
+            this.Controls.Add(this.skis);
+            this.Controls.Add(this.snowgloves);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Flyers Sports Club Sign-up Sheet";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox snowgloves;
+        private System.Windows.Forms.CheckBox skis;
+        private System.Windows.Forms.CheckBox goggles;
+        private System.Windows.Forms.CheckBox earmuffs;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem cartToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clearOrderToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitApplicationToolStripMenuItem;
+    }
+}
+
---
+++ SkiSignupsheet/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SkiSignupsheet
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exitApplicationToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void clearOrderToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            foreach (var control in Controls)
+            {
+                if (control is CheckBox checkbox)
+                {
+                    checkbox.Checked = false;
+                }
+            }
+        }
+
+        private void cartToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var selecteditems = Controls.OfType<CheckBox>()
+                .Where(cb => cb.Checked)
+                .Select(cb => cb.Text)
+                .ToList();
+
+            if (selecteditems.Any())
+            {
+                MessageBox.Show("Selected items: " + string.Join(", ", selecteditems), "Selected Items");
+            }
+            else
+            {
+                MessageBox.Show("No items selected.", "Selected Items");
+            }
+        }
+    }
+}
